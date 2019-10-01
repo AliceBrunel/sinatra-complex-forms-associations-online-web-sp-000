4943cdd467241f3519c5101d1825058dc2827a15
Alice Brunel <alc.brunel@gmail.com>
2019-10-01 12:27:11 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -42,9 +42,13 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
     # Remove all previous owner    
+    
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+    
+    @pet.save
+    
     redirect "pets/#{@pet.id}"
   end
 end
