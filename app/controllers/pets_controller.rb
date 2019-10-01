d0e4b82e72976134d896638ef5f601bb7c4d2feb
Alice Brunel <alc.brunel@gmail.com>
2019-10-01 11:11:38 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,7 +15,7 @@ class PetsController < ApplicationController
       if !params["owner"]["name"].empty?
         @pet.owner << Owner.create(name: params["owner"]["name"])
       end
-    redirect "/owners/#{@owner.id}"
+    redirect "/pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
